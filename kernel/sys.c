a9dd75a067e26b3cc8ccab4af1ab2d0899b0ebaa
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-02-05 21:22:32 +0800

    fs: Implement SUSFS v1.5.5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1184,6 +1184,9 @@ static int override_release(char __user *release, size_t len)
 
 
 static uint64_t netbpfload_pid = 0;
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1199,6 +1202,11 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 				 current->comm, current->pid, tmp.release);
 		}
 	}
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+    else {
+	    susfs_spoof_uname(&tmp);
+    }
+#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
