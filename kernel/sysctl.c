7ac112885593a55addc5abaee79e9c47064bc3fd
Tolga Uçar <freetolga@protonmail.com>
2025-03-18 06:23:27 +0000

    fix even more merging mistakes

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1937,6 +1937,7 @@ static struct ctl_table vm_table[] = {
 		.maxlen		= sizeof(sysctl_swap_ratio_enable),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
+  },
 #ifdef CONFIG_USERFAULTFD
 	{
 		.procname	= "unprivileged_userfaultfd",
